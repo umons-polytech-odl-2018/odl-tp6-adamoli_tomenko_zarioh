d431b33f9bceb83ac5018285d8f29c31c5b4839c
Zyvrox <mehdi.zarioh@gmail.com>
2018-12-07 15:43:36 +0100

    factory

--- src/main/java/Carbonara_Hut.java
+++ src/main/java/Carbonara_Hut.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Carbonara_Hut extends Carbonara_Domino {
+    public Carbonara_Hut(ArrayList<Ingredient> ingredients){
+        super(ingredients);
+        ingredients.add(new Olives());
+    }
 }
--- src/main/java/Margherita_Hut.java
+++ src/main/java/Margherita_Hut.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class Margherita_Hut extends Margherita_Domino {
     public Margherita_Hut(ArrayList<Ingredient> ingredients){
         super(ingredients);
-        ingredients.
+        ingredients.add(new Olives());
     }
 }
