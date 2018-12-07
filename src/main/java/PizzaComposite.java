56c4123ebc3204740dd4af17b187d2073793a1b9
PTom8 <171567@umons.ac.be>
2018-12-07 13:52:15 +0100

    zsef

--- src/main/java/PizzaComposite.java
+++ src/main/java/PizzaComposite.java
@@ -1,2 +1,40 @@
-public class PizzaComposite {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class PizzaComposite implements Composant {
+
+    private Collection children;
+
+    public PizzaComposite() {
+        children = new ArrayList();
+    }
+
+
+    public void add(Composant composant){
+
+        children.add(composant);
+    }
+
+
+    public void remove(Composant composant){
+        children.remove(composant);
+    }
+
+    public Iterator getChildren() {
+        return children.iterator();
+    }
+
+    @Override
+    public int getPrize() {
+        int result = 0;
+        for (Iterator i = children.iterator(); i.hasNext(); ) {
+            Object objet = i.next();
+
+            Composant composant = (Composant)objet;
+
+            result += composant.getPrize();
+        }
+        return result;
+    }
 }
